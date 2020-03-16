2267e8ba786336e046fac787027a969b41620d9e
medic-heal <62261590+medic-heal@users.noreply.github.com>
2020-03-17 03:02:44 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+TOKEN = 'NjcwODkyNDg3OTU4NTkzNTM2.Xi0_dA.x4bqUERvY2hWyutb2LtWeiufOYM' # bot token
+
+POST_ID = 689178235975499886 # post id to read reactions from
+
+# roles list according to emotes
+ROLES = {
+	'🍗': 647843443015680032, # Мясистый
+	'🍌': 647839895695654922, # Сладкий банан
+	'🍔': 647839545072680995, # Бутербродный
+	'🎂': 647843688395046936, #праздничный
+	'🤖': 647839639691853824, #Бутербродно-подобный
+	'🍫': 647839720482668554, #йындалокоШ
+	'🐟': 647839799998414848 #Селёдка под шубой
+
+} 
+
+# exclude this roles from couting
+EXCROLES = {}
+
+MAX_ROLES_PER_USER = 2 # max ammount of roles a use can have
